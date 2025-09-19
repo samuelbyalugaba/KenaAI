bcf39721e6ea91024fa5e180092ce891d8690a6c
Samuel Byalugaba <sammyjerry5@gmail.com>
2025-09-19 07:18:18 +0000

    Error: A tree hydrated but some attributes of the server rendered HTML d

--- src/components/ui/theme-provider.tsx
+++ src/components/ui/theme-provider.tsx
@@ -5,18 +5,6 @@ import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import type { ThemeProviderProps as NextThemesProviderProps } from "next-themes/dist/types"
 
-import { useTheme as useNextTheme } from "next-themes"
-
-export type Theme = "dark" | "light" | "system"
-
-export type ThemeContextType = {
-  theme: Theme
-  setTheme: (theme: Theme) => void
-}
-
-// Re-export useTheme from next-themes
-export const useTheme = useNextTheme
-
 export function ThemeProvider({ children, ...props }: NextThemesProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
